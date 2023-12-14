dbb76e6b6b7bc067a01daf0451605b8ad0ccd44d
TheGloved1 <gloves1229@gmail.com>
2023-12-14 17:24:06 -0600

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import discord
-from discord import BotIntegration, Message as DiscordMessage
+from discord import BotIntegration, Message as DiscordMessage, MessageReference
 import logging
 import asyncio
 import json
 import os
-from discord.ext import commands
 from uuid import uuid4
 from time import time
 from datetime import datetime
@@ -74,7 +73,12 @@ async def on_ready():
         completion.MY_BOT_EXAMPLE_CONVOS.append(Conversation(messages=messages))
     await tree.sync()
 
-
+def sendMessage(message: DiscordMessage, content: str):
+    TextChannel = message.channel.type == discord.ChannelType.text
+    if TextChannel:
+        return message.reply(content)
+    else:
+        return message.channel.send(content)
 
 # calls for each message
 @client.event
@@ -86,109 +90,99 @@ async def on_message(message: DiscordMessage):
     channel = message.channel
     TextChannel = channel.type == discord.ChannelType.text
     MentionsBot = client.user.mentioned_in(message)
-    content = ":thinking: Thinking..."
-    if (TextChannel):
-        interactive_response = await message.reply(content)
-        if not (MentionsBot):
-            if not (channel.name == 'gloved-gpt'):
-                return
-    else:
-        interactive_response = await channel.send(content)
-    # Start off the response message, this'll be the one we keep updating
-    
+    MentionContent = message.content.removeprefix('<@938447947857821696>')
 
-    try:
-        if message.content == "!resetchannel":
-            if not TextChannel:
-                return
-            channel_position = channel.position
-            
-            # Clone the channel
-            new_channel = await channel.clone(reason="Channel reset")
-            await new_channel.edit(position=channel_position)
-
-            # Delete the original channel
-            await channel.delete(reason="Channel reset by command")
-            # Send confirmation to the new channel
-            await new_channel.send("Channel has been reset. Not a trace left, like my last user's dignity.")
+    if message.content == "!resetchannel":
+        if not TextChannel:
             return
+        channel_position = channel.position
+        
+        # Clone the channel
+        new_channel = await channel.clone(reason="Channel reset")
+        await new_channel.edit(position=channel_position)
+
+        # Delete the original channel
+        await channel.delete(reason="Channel reset by command")
+        # Send confirmation to the new channel
+        await new_channel.send("Channel has been reset. Not a trace left, like my last user's dignity.")
+        return
+        
+    try:
+
         # block servers not in allow list
+        if message.content.startswith('!' or '?' or '.'):
+            return
         if (TextChannel):
             if not (MentionsBot):
                 if not (channel.name == 'gloved-gpt'):
                     return
                 
-        async with channel.typing():
+        thinkingText = "`Processing Message...`"
+        interactive_response = await sendMessage(message, thinkingText)
+        # Start off the response message, this'll be the one we keep updating
+    
+        # save message as embedding, vectorize
+        vector = gpt3_embedding(message)
+        timestamp = time()
+        timestring = timestring = timestamp_to_datetime(timestamp)
+        user = message.author.name
+        extracted_message = '%s: %s - %s' % (user, timestring, MentionContent)
+        info = {'speaker': user, 'timestamp': timestamp,'uuid': str(uuid4()), 'vector': vector, 'message': extracted_message, 'timestring': timestring}
+        filename = 'log_%s_user' % timestamp
+        save_json(f'./src/chat_logs/{filename}.json', info)
+
+        # load past conversations
+        history = load_convo()
+
+        # fetch memories (histroy + current input)
+
+        thinkingText = "`Loading Memories...`"
+        await interactive_response.edit(content = thinkingText)
+
+        memories = fetch_memories(vector, history, 5)
+
+        # create notes from memories
+
+        current_notes, vector = summarize_memories(memories)
+
+        print(current_notes)
+        print('-------------------------------------------------------------------------------')
+
+        add_notes(current_notes)
+
+        if len(notes_history) >= 2:
+            print(notes_history[-2])
+        else:
+            print("The list does not have enough elements to access the second-to-last element.")
+
+
+        # create a Message object from the notes
+        message_notes = Message(user='memories', text=current_notes)
         
-            # save message as embedding, vectorize
-            vector = gpt3_embedding(message)
-            timestamp = time()
-            timestring = timestring = timestamp_to_datetime(timestamp)
-            user = message.author.name
-            extracted_message = '%s: %s - %s' % (user, timestring, message.content)
-            info = {'speaker': user, 'timestamp': timestamp,'uuid': str(uuid4()), 'vector': vector, 'message': extracted_message, 'timestring': timestring}
-            filename = 'log_%s_user' % timestamp
-            save_json(f'./src/chat_logs/{filename}.json', info)
+        # create a Message object from the notes_history for context
 
-            # load past conversations
-            history = load_convo()
-
-            # fetch memories (histroy + current input)
-
-            memories = fetch_memories(vector, history, 5)
-
-            # create notes from memories
-
-            current_notes, vector = summarize_memories(memories)
-
-            print(current_notes)
-            print('-------------------------------------------------------------------------------')
-
-            add_notes(current_notes)
-
-            if len(notes_history) >= 2:
-                print(notes_history[-2])
-            else:
-                print("The list does not have enough elements to access the second-to-last element.")
+        context_notes = None
+        
+        if len(notes_history) >= 2:
+            context_notes = Message(user='context', text=notes_history[-2])
+        else:
+            print("The list does not have enough elements create context")
 
 
-            # create a Message object from the notes
-            message_notes = Message(user='memories', text=current_notes)
-            
-            # create a Message object from the notes_history for context
-
-            context_notes = None
-            
-            if len(notes_history) >= 2:
-                context_notes = Message(user='context', text=notes_history[-2])
-            else:
-                print("The list does not have enough elements create context")
-
-
-            # wait a bit in case user has more messages
-            # if SECONDS_DELAY_RECEIVING_MSG > 0:
-            #     await asyncio.sleep(SECONDS_DELAY_RECEIVING_MSG)
-            #     if is_last_message_stale(
-            #         interaction_message=message,
-            #         last_message=channel.last_message,
-            #         bot_id=client.user.id,
-            #     ):
-            #         # there is another message, so ignore this one
-            #         return
-
-            logger.info(
-                f"Message to process - {message.author}: {message.content[:50]} - {channel.id} {channel.jump_url}"
-            )
-
-            channel_messages = [
-                discord_message_to_message(message)
-                async for message in channel.history(limit=MAX_MESSAGE_HISTORY)
-            ]
-            channel_messages = [x for x in channel_messages if x is not None]
-            channel_messages.reverse()
-            channel_messages.insert(0, message_notes)
-            if context_notes:
-                channel_messages.insert(0, context_notes)
+        logger.info(
+            f"Message to process - {message.author}: {message.content[:50]} - {channel.id} {channel.jump_url}"
+        )
+        thinkingText = "`Reading Previous Messages...`"
+        await interactive_response.edit(content = thinkingText)
+        channel_messages = [
+            discord_message_to_message(message)
+            async for message in channel.history(limit=MAX_MESSAGE_HISTORY)
+        ]
+        channel_messages = [x for x in channel_messages if x is not None]
+        channel_messages.reverse()
+        channel_messages.insert(0, message_notes)
+        if context_notes:
+            channel_messages.insert(0, context_notes)
 
         # generate the response
         timestamp = time()
@@ -203,34 +197,39 @@ async def on_message(message: DiscordMessage):
         
         rendered = prompt.render()
         print(rendered)
-        
+
+        thinkingText = "`Creating Response...` **--->** "
+        await interactive_response.edit(content = thinkingText)
+
         response = openai.ChatCompletion.create(
-            model="gpt-4-1106-preview",
+            model="gpt-4",
             messages=[{"role": "system", "content": rendered}],
             stream=True
         )
 
-        # content = "Thinking..."
-        # interactive_response = await channel.send(content)
         collected_chunks = []
         collected_messages = []
 
         # Fetch chunks from the stream
+        logger.info('Getting chunks...')
         for chunk in response:
+            await asyncio.sleep(.5)
             collected_chunks.append(chunk)
             chunk_message = chunk['choices'][0]['delta']
             collected_messages.append(chunk_message)
             full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
             if full_reply_content and not full_reply_content.isspace():
+                await interactive_response.edit(content = thinkingText + full_reply_content)
+            if len(full_reply_content) > 1925:
                 await interactive_response.edit(content = full_reply_content)
-            if len(full_reply_content) > 1500:
                 logger.info(full_reply_content)
-                interactive_response = await channel.send(content)
+                interactive_response = await channel.send(thinkingText)
                 collected_messages = [] 
 
             
             
-            await asyncio.sleep(0.1) # Throttle the loop to avoid rate limits
+            await asyncio.sleep(0.5) # Throttle the loop to avoid rate limits
+        await interactive_response.edit(content = full_reply_content)
         logger.info(full_reply_content)
             # response_data = await generate_completion_response(
             #     messages=channel_messages, user=message.author, message=message
